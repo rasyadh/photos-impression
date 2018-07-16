3b6bd09ad00a574b064844c1be5598dcf0231af7
RASYADH\Rasyadh A Aziz <rasyadhabdulaziz@gmail.com>
2018-07-16 13:10:47 +0700

    test svm with kernel poly

--- project/testing/facial_expression_recognition.py
+++ project/testing/facial_expression_recognition.py
@@ -35,7 +35,7 @@ param_grid = {
     'C': [1e3, 5e3, 1e4, 5e4, 1e5],
     'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]
 }
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+clf = GridSearchCV(SVC(kernel='poly', class_weight='balanced'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
 print(clf.best_estimator_)
 
